4f4c757f6615d65d2718717625741788b15a0d9b
Clément Péron <peron.clem@gmail.com>
2020-04-19 11:14:47 +0200

    drm/panfrost: properly handle error in probe
    Introduce a boolean to know if opp table has been added.
    
    With this, we can call panfrost_devfreq_fini() in case of error
    and release what has been initialised.
    
    Signed-off-by: Clément Péron <peron.clem@gmail.com>

--- drivers/gpu/drm/panfrost/panfrost_devfreq.h
+++ drivers/gpu/drm/panfrost/panfrost_devfreq.h
@@ -14,6 +14,7 @@ struct panfrost_device;
 struct panfrost_devfreq {
 	struct devfreq *devfreq;
 	struct thermal_cooling_device *cooling;
+	bool opp_of_table_added;
 
 	ktime_t busy_time;
 	ktime_t idle_time;
